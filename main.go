02085bcb994cbbf2ac701c2eaa0bbfe47284dff8
jay-mckay <jackjaymckay@gmail.com>
2023-11-03 17:13:21 -0600

    add initial tcell structure

--- main.go
+++ main.go
@@ -3,29 +3,50 @@ package main
 import (
   "fmt"
   "os"
-  "os/exec"
+  
+  "github.com/gdamore/tcell/v2"
+  "github.com/gdamore/tcell/v2/encoding"
 )
 
-// TODO: look into tcell
 
-var up byte = 65
-var down byte = 66
-var right byte = 67
-var left byte = 68
+func check(e error) {
+  if e != nil {
+    fmt.Fprintf(os.Stderr, "%v\n", e)
+    os.Exit(1)
+  }
+}
 
 func main() {
-  // no input buffering
-  exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-  // don't display characters entered
-  exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+  encoding.Register()
+
+  screen, err := tcell.NewScreen()
+  check(err)
+
+  err = screen.Init()
+  check(err)
+
+  defaultStyle := tcell.StyleDefault.
+  	Background(tcell.ColorBlack).
+	Foreground(tcell.ColorWhite)
+
+  screen.SetStyle(defaultStyle)
+	
+  width, height := screen.Size()
+  screen.Clear()
+  newStyle := tcell.StyleDefault.
+  	Background(tcell.ColorWhite).
+	Foreground(tcell.ColorCadetBlue.TrueColor())
   
-  var b []byte = make([]byte, 1)
+  screen.SetContent(width/2, height/2, 'H', nil, newStyle)
+  screen.Show()
+
   for {
-    os.Stdin.Read(b)
-    if b[0] == up {
-    } else if b[0] == down {
-    } else if b[0] == right {
-    } else if b[0] == left {
+    switch event := screen.PollEvent().(type) {
+      case *tcell.EventKey:
+        if event.Key() == tcell.KeyEscape {  
+	  screen.Fini()
+	  os.Exit(0)
+      }
     }
   }
 }
